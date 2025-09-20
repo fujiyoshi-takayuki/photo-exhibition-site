288fce00cadb8bba4c38fbc1efd5d66a566291a6
Fujiyoshi Takayuki <takayuki.fujiyoshi.2r@stu.hosei.ac.jp>
2025-09-20 23:06:51 +0900

    update header and navbar

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
 let lastScrollY = window.scrollY;
 const header = document.getElementById("main-header");
+const navbar = document.querySelector(".navbar");
 
 window.addEventListener("scroll", () => {
-    if (window.scrollY <= 0) {
-        // ページ最上部では常に表示
-        header.style.top = "0";
-    } elese if (window.scrollY > lastScrollY) {
-        // 下スクロール → ヘッダーを隠す
-        header.style.top = "70px";
-    } else {
-        // 上スクロール → ヘッダーを表示
-        header.style.top = "0";
-    }
-    lastScrollY = window.scrollY;
+  if (window.scrollY <= 0) {
+    // ページ最上部
+    header.style.top = "0";
+    navbar.style.top = "70px"; // ヘッダーの下
+  } else if (window.scrollY > lastScrollY) {
+    // 下スクロール → ヘッダー隠す
+    header.style.top = "-70px";
+    navbar.style.top = "0"; // ナビが最上部に吸い付く
+  } else {
+    // 上スクロール → ヘッダー表示
+    header.style.top = "0";
+    navbar.style.top = "70px"; // 押し出されて下にずれる
+  }
+  lastScrollY = window.scrollY;
 });
