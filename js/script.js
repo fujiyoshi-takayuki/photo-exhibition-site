4ed02960c5f205e9fae0f4379c5a5f900bb7be21
Fujiyoshi Takayuki <takayuki.fujiyoshi.2r@stu.hosei.ac.jp>
2025-09-20 22:19:40 +0900

    reset header.style

--- js/script.js
+++ js/script.js
@@ -2,9 +2,14 @@ let lastScrollY = window.scrollY;
 const header = document.getElementById("main-header");
 
 window.addEventListener("scroll", () => {
-    if (window.scrollY > lastScrollY) {
-        header.style.top = "-70px"; // ヘッダーの高さ分隠す
+    if (window.scrollY <= 0) {
+        // ページ最上部では常に表示
+        header.style.top = "0";
+    } elese if (window.scrollY > lastScrollY) {
+        // 下スクロール → ヘッダーを隠す
+        header.style.top = "70px";
     } else {
+        // 上スクロール → ヘッダーを表示
         header.style.top = "0";
     }
     lastScrollY = window.scrollY;
